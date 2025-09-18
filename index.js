8b675763e8063ac2a78f2d2dd3f47053f2b1d140
Sayali-189 <sayali.khade@rigvedit.com>
2025-09-18 16:57:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello from Node.js app!'));
+app.get('/health', (req, res) => res.send('Health check!'));
 
 app.listen(port, () => console.log(`App running on port ${port}`));
